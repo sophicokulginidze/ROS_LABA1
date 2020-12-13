c03ef3d7b5ffa43bddc56a614a94020de0953892
sophicokulginidze <43095579+sophicokulginidze@users.noreply.github.com>
2020-12-13 15:51:16 +0300

    По красоте

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -4,38 +4,38 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <iterator>
 #include <cctype>
 
 using namespace std;
 
+ros::Publisher pub;
+
 void recieve(const std_msgs::String & msg) {
   std_msgs::String str;
   str.data = msg.data;
-  transform(str.begin(), str.end(), str.begin(), tolower);
-  for (int i = 0; i < strlen(str); i++) 
-  {
-    if (str[i] == " ")
-    {
+  std::transform(str.data.begin(), str.data.end(), str.data.begin(), [](unsigned char c) {return std::tolower(c); });
+  for (ulong i = 0; i < str.data.length(); i++) {
+    if (str.data[i] == ' ') {
       i++;
-    } else
-    {
-      if (!(i % 2)) 
-      {
-        str[i] = toupper(str[i]);
-      } else 
-      {
-        str[i] = str[i];
+    } else {
+      if (!(i % 2)) {
+        str.data[i] = toupper(str.data[i]);
+      } else {
+        str.data[i] = str.data[i];
       }
     }
   }
-  ROS_INFO("Result string: %s", str.data);
+  ROS_INFO_STREAM("Result string:" << str.data);
+  pub.publish(str);
   return;
 }
 
 int main(int argc, char ** argv) {
   ros::init(argc, argv, "subscriber");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/string_out", 1, recieve);
+  ros::Subscriber sub = n.subscribe("/string_in", 1, recieve);
+  pub = n.advertise<std_msgs::String>("/string_out",1);
   ros::spin();
   return 0;
 }
